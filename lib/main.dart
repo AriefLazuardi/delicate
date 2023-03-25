85265467d2842685c5408aafb414998e6615ee06
AriefLazuardi <h1101211048@student.untan.ac.id>
2023-03-25 13:41:42 +0700

    .

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:belajar_flutter/pages/pertama/pertama.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: WelcomePage(),
+        routes: {
+          "/pertama": (context) => Login(),
+        });
+  }
+}
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffF4F4F4),
+      body: ListView(
+        children: [
+          Text("Temukan masakan tepat untuk momen acaramu di Delicate"),
+          TextButton(
+              onPressed: () => Navigator.pushNamed(context, "/pertama"),
+              child: Text("Masuk"))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/pertama/pertama.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Delicate"),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: 314,
+            height: 42,
+            color: Color(0xffC21010),
+            child: Text("Mulai"),
+          )
+        ],
+      ),
+    );
+  }
+}
