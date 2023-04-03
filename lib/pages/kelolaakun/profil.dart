2e643bf430be99197618376ac281f14f9a4c2447
Phillip-Wongsodinata <h1101211005@student.untan.ac.id>
2023-04-03 13:33:15 +0700

    .

--- lib/pages/kelolaakun/profil.dart
+++ lib/pages/kelolaakun/profil.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:delicate/shared/shared.dart';
 import 'package:flutter/material.dart';
 
@@ -15,48 +13,21 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: whiteColor,
-      body: ListView(children: [
-        Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          alignment: Alignment.bottomCenter,
-          decoration: const BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/pattern.png"),
-                  fit: BoxFit.cover)),
-        ),
-        Container(
-          alignment: Alignment.center,
-          child: Image(image: AssetImage("assets/images/delicate.png")),
-        ),
-        Container(
-          decoration:
-              BoxDecoration(borderRadius: BorderRadius.circular(4), boxShadow: [
-            BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                offset: Offset(0, 2),
-                blurRadius: 5)
-          ]),
-          margin: EdgeInsets.fromLTRB(20, 0, 20, 35),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Column(
-                children: [
-                  Text(
-                    "Rina Novanti",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: primaryColor,
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        ),
-      ]),
+      body: ListView(
+        children: [
+          Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Column(
+ 
+                )
+             
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
