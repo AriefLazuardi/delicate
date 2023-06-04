d4c8017d92153ea5847919c81231e7535166f9b2
AriefLazuardi <h1101211048@student.untan.ac.id>
2023-06-04 20:35:14 +0700

    salah

--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 class Palatte {
-  static String sUrl = "https://1273-118-99-118-170.ngrok-free.app/api";
+  static String sUrl = "https://638e-36-85-116-165.ngrok-free.app/api";
 }
